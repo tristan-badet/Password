0dd61edab5943ffd8f6fd5011e0354b801ac1e26
tristan-badet <127488323+tristan-badet@users.noreply.github.com>
2023-03-13 13:58:42 +0100

    Amélioration et répétition des étapes si mot de passe faux

--- Password.py
+++ Password.py
@@ -20,7 +20,22 @@ for char in mdpUtilisateur:
     elif not char.isalnum():
         mdpSpecial = True
 
-if mdpLenght == True and mdpLower == True and mdpUpper == True and mdpNumber == True and mdpSpecial == True:
-    print("Votre mot de passe respecte les exigences de sécurité")
+while mdpLenght == False or mdpUpper == False or mdpLower == False or mdpNumber == False or mdpSpecial == False:
+    print("Votre mot de passe ne répond pas à toutes les exigences de sécurité")
+    mdpUtilisateur = str(input("Veuillez réessayer un autre mot de passe : "))
+    if len(mdpUtilisateur) > 7:
+        mdpLenght = True
+    else:
+        mdpLenght = False
+    for char in mdpUtilisateur:
+        if char.islower():
+            mdpLower = True
+        elif char.isupper():
+            mdpUpper = True
+        elif char.isdigit():
+            mdpNumber = True
+        elif not char.isalnum():
+            mdpSpecial = True
 else:
-    print("Erreur, le mot de passe ne respecte pas les exigences de sécurité")
+    print("Votre mot de passe répond aux exigences de sécurité")
+   
